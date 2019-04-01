c2e3988231fdc1b2b573abbe48ae079f8749dee5
luca29092002 <48947960+luca29092002@users.noreply.github.com>
2019-04-01 16:15:04 +0200

    Add files via upload

---
+++ WindowsFormsApp12/WindowsFormsApp12/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WindowsFormsApp12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(644, 351);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormsApp12/WindowsFormsApp12/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.IO;
+using System.Speech.Synthesis;
+using System.Threading;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine sentitizzatore = new SpeechRecognitionEngine();
+        SpeechSynthesizer jarvis = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+            sentitizzatore.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Sentitizzatore_SpeechRecognized);
+            LoadGrammar();
+            sentitizzatore.SetInputToDefaultAudioDevice();
+            sentitizzatore.RecognizeAsync(RecognizeMode.Multiple);
+        }
+        public void LoadGrammar()
+        {
+
+            Choices testo = new Choices();
+            string[] linea = File.ReadAllLines(Environment.CurrentDirectory + "//comandi.txt");
+            testo.Add(linea);
+            Grammar newlista = new Grammar(new GrammarBuilder(testo));
+            sentitizzatore.LoadGrammar(newlista);
+
+        }
+        public void Sentitizzatore_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            richTextBox1.Text = e.Result.Text;
+            string parola = e.Result.Text;
+            if (parola == "ciao")
+            {
+                jarvis.Speak("ciao come va");
+            }
+            if (parola == "e tu")
+            {
+                jarvis.Speak("bene grazie");
+            }
+            if (parola == "ciao")
+            {
+                jarvis.Speak("ciao, coma va");
+            }
+            if (parola == "apri google")
+            {
+                jarvis.Speak("losto aprendo");
+                System.Diagnostics.Process.Start("http://www.google.com");
+            }
+        }
+    }
+}
